28beadf333fe9623a7338bdfc0b61477c2d285f6
aashikur <mdaashikur@gmail.com>
2025-03-04 15:28:53 +0600

    js connected

---
+++ scripts/validation.js
@@ -0,0 +1,12 @@
+// Global Vaiable
+
+console.log("===================================");
+const userPwd = document.getElementById('userPwd');
+
+
+
+userPwd.addEventListener('keyup',function(){
+    console.log(userPwd.value);
+})
+
+
