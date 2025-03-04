4b7ed574354adad27367802be3087baf6e973591
aashikur <mdaashikur@gmail.com>
2025-03-04 17:28:36 +0600

    lol

--- scripts/validation.js
+++ scripts/validation.js
@@ -2,11 +2,22 @@
 
 console.log("===================================");
 const userPwd = document.getElementById('userPwd');
+console.log(userPwd.length, 'top')
 
 
 
 userPwd.addEventListener('keyup',function(){
-    console.log(userPwd.value);
+    const pwd = userPwd;
+    checklength(userPwd); 
+
+    
 })
 
 
+
+
+// Check Length
+function checklength(e){
+    if(e.value.length >= 8)
+        console.log(e.value.length);
+}
