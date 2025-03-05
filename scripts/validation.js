4d0285ed2e4d9a681fa6f6bd104fe862b13a79c5
aashikur <mdaashikur@gmail.com>
2025-03-05 19:56:00 +0600

    litle adject

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,84 +1,73 @@
-// Global Vaiable
-
-console.log("===================================");
-const getId = (id) => document.getElementById(id);
-
+// Global Variable 
+const ID = (id) => document.getElementById(id);
+const warningBox = ID('warningBox');
+const warningBoxText = document.querySelectorAll("#warningBox li")
+const userGivenPassword = ID('userPwd');
 
 
 // Main Function
-getId('userPwd').addEventListener('keyup',function(){
-    
-    
-    checklength(userPwd,'digits8'); 
-    checkCapital(userPwd,'capital1'); 
-
-    
+userGivenPassword.addEventListener('keyup',(event)=>{
+   warningTextEightDigits();
 })
 
+console.log(warningBoxText[0].childNodes);
+warningBoxText[0].firstChild.style.backgroundColor = 'yellow';
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//  Check For if Number  is 8 Digits
-function checklength(inputField,worningTextID){
-    const condition = inputField.value.length >= 8
-    worningTextChange(worningTextID, condition);
-}
-
-//  Check For if Contain A-Z
-function checkCapital(inputField,worningTextID){
-    const condition = /[A-Z]/.test(inputField);
-    worningTextChange(worningTextID, condition);
-}
-
-
-
-// Worining Text Changes 
-function worningTextChange(id,condition){
-    const selectedTextField = document.getElementById(id);
-
-    if(condition)
-    {
-        selectedTextField.style.opacity = '1';
-        selectedTextField.querySelector('span').style.backgroundColor = '#86ff86';
-    } 
-    else {
-        selectedTextField.style.opacity = '0.35';
-        selectedTextField.style.color = '#ddd';
-        selectedTextField.querySelector('span').style.backgroundColor = '#fb2c36';
-    }
+function warningTextEightDigits(){
     
 }
+
+
+
+
+
+
+
+// // Global Vaiable
+
+// console.log("===================================");
+// const getId = (id) => document.getElementById(id);
+
+
+
+// // Main Function
+// getId('userPwd').addEventListener('keyup',function(){
+    
+    
+//     checklength(userPwd,'digits8'); 
+//     checkCapital(userPwd,'capital1'); 
+
+    
+// })
+
+
+// //  Check For if Number  is 8 Digits
+// function checklength(inputField,worningTextID){
+//     const condition = inputField.value.length >= 8
+//     worningTextChange(worningTextID, condition);
+// }
+
+// //  Check For if Contain A-Z
+// function checkCapital(inputField,worningTextID){
+//     const condition = /[A-Z]/.test(inputField);
+//     worningTextChange(worningTextID, condition);
+// }
+
+
+// // Worining Text Changes 
+// function worningTextChange(id,condition){
+//     const selectedTextField = document.getElementById(id);
+
+//     if(condition)
+//     {
+//         selectedTextField.style.opacity = '1';
+//         selectedTextField.querySelector('span').style.backgroundColor = '#86ff86';
+//     } 
+//     else {
+//         selectedTextField.style.opacity = '0.35';
+//         selectedTextField.style.color = '#ddd';
+//         selectedTextField.querySelector('span').style.backgroundColor = '#fb2c36';
+//     }
+    
+// }
